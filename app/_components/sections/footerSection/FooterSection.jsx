e13b5eedcd113be4217ad44ed0921f2c74390dd6
FL45h-09 <vishal.ravanank@gmail.com>
2025-06-20 16:17:23 +0530

    🔥 Initial commit – Clean slate

--- app/_components/sections/footerSection/FooterSection.jsx
+++ app/_components/sections/footerSection/FooterSection.jsx
@@ -5,7 +5,7 @@ export const FooterSection = () => {
   return (
     <div className='py-4 text-center'>
         <div className="container">
-            <p className="text-sm text-center text-gray-500 dark:text-gray-400">🪐👨‍🚀 Space-Online is an open-source project by <Link href="https://github.com/FL45h-09" className="underline hover:text-indigo-400 transition-colors">Vishal Ravanank</Link> — explore, fork, and contribute.</p>
+            <p className="text-sm text-center text-gray-500 dark:text-gray-400">🪐👨‍🚀 Online Space is an open-source project by <Link href="https://github.com/FL45h-09" className="underline hover:text-indigo-400 transition-colors">Vishal Ravanank</Link> — explore, fork, and contribute.</p>
         </div>
     </div>
   )
